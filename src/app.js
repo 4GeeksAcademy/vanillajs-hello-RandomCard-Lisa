299da6a87d5732e1804ef6b7099601ab12141900
Lisa Valdes <132381289+LisaMV@users.noreply.github.com>
2023-06-21 03:01:20 +0000

    Random Card done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let Allvalues = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
+let suits = ["♦", "♥", "♠", "♣"];
+
+function Loadvalues(arr1) {
+  return arr1[Math.floor(Math.random() * arr1.length)];
+}
+
+function Loadsuits(arr2) {
+  return arr2[Math.floor(Math.random() * arr2.length)];
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(Loadvalues(Allvalues));
+  //console.log(Loadsuits(suits));
+
+  const pTagHtmlElement = document.querySelector("#values");
+  pTagHtmlElement.innerHTML = Loadvalues(Allvalues);
+  const pTagHtmlElement1 = document.querySelector("#Suit-left");
+  const pTagHtmlElement2 = document.querySelector("#Suit-right");
+  pTagHtmlElement1.innerHTML = Loadsuits(suits);
+  pTagHtmlElement2.innerHTML = pTagHtmlElement1.innerHTML;
 };
